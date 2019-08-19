5566e617ccc1a1a0570fd629816629f68e75de7c
Yordin <yordiaguilar17one@gmail.com>
2019-08-19 13:58:35 -0500

    1er commit, seccion 3 VUEJS

---
+++ xborar01/CursoVuejs/js/principal.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+    el : '#app',
+    data : {
+        lista : [
+        ],
+        nombre : '',
+        promedio : ''
+    },
+    methods : {
+        agregarNota: function (){
+            estadoA=false;
+            if (this.promedio>=12.5){
+                estadoA=true;
+            }
+            if (this.nombre!="" && this.promedio!=""){
+                this.lista.push({nombre:this.nombre, promedio:this.promedio, estado:estadoA});
+                this.nombre="";
+                this.promedio="";
+            }
+            else{
+                alert ("Ingrese el nombre y promedio del estudiante");
+            }
+        }
+    }
+})
